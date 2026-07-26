1cfb32f3f900f52ab1c47c879929b7f53419ff61
bogus <rukscbogumil@gmail.com>
2026-07-26 21:22:01 +0200

    Tests and solution

---
+++ CrmClientsManager/CrmClientsManager.Tests/CustomerServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using CrmClientsManager.Application.Services;
+using CrmClientsManager.Domain.Entities;
+using CrmClientsManager.Infrastructure.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CrmClientsManager.Tests
+{
+    public class CustomerServiceTests
+    {
+        [Fact]
+        public void Add_ShouldThrow_WhenNameIsEmpty()
+        {
+            var repository =
+                new Mock<CustomerRepository>();
+
+            var service =
+                new CustomerService(repository.Object);
+
+
+            var customer = new Customer
+            {
+                Name = "",
+                Nip = "1234567890",
+                Email = "test@test.com"
+            };
+
+
+            Assert.Throws<Exception>(() =>
+                service.Create(customer));
+        }
+
+        [Fact]
+        public void Add_ShouldThrow_WhenNipIsInvalid()
+        {
+            var repository =
+                new Mock<CustomerRepository>();
+
+            var service =
+                new CustomerService(repository.Object);
+
+
+            var customer = new Customer
+            {
+                Name = "Test",
+                Nip = "123",
+                Email = "test@test.com"
+            };
+
+
+            Assert.Throws<Exception>(() =>
+                service.Create(customer));
+        }
+    }
+}
